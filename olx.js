d4d36694abd72ab9354577e4ff7f234e4afbf4fd
Gunjan Naithani <103548877+Gunjannaithani@users.noreply.github.com>
2022-11-28 09:54:18 +0530

    update

--- olx.js
+++ olx.js
@@ -5,6 +5,18 @@ const changeMode = () => {
 }
 
 
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
   /*coupon  */
 function loadcoupon()
 {
